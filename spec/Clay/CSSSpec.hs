fe859d436917426308bd664172465d0c8d0411d5
Martin <martinjonsson01@gmail.com>
2024-03-13 09:13:35 +0100

    Generate more properties
    Co-authored-by: Anton Ekström <antoneekstrom@users.noreply.github.com>

--- spec/Clay/CSSSpec.hs
+++ spec/Clay/CSSSpec.hs
@@ -1,19 +1,17 @@
 module Clay.CSSSpec where
 
-import           Clay
-import           Clay.AspectRatio
-import           Common
-import           Data.Ratio
-import           Test.Hspec
+import Clay
 import Data.Either
-import Text.CSS.Parse ( parseNestedBlocks )
-import Test.Hspec.QuickCheck
 import qualified Data.Text.Lazy as Lazy
 import Generators
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Text.CSS.Parse (parseNestedBlocks)
 
 spec :: Spec
 spec = do
   describe "Css" $ do
-    prop "generated css can be parsed correctly" $
-      \(ArbitraryCss css) -> let rendered = Lazy.toStrict $ render css in
-      isRight $ parseNestedBlocks rendered
+    prop "generated css can be parsed correctly"
+      $ \(ArbitraryCss css) ->
+        let rendered = Lazy.toStrict $ render css
+         in isRight $ parseNestedBlocks rendered
--- spec/Generators.hs
+++ spec/Generators.hs
@@ -8,10 +8,14 @@ import           Test.QuickCheck
 import Prelude hiding ((**))
 import Clay.Stylesheet
 import Clay.Render
-import Clay.Geometry
+import Clay.Geometry hiding (size)
 import Data.Ratio
 import Data.Bifunctor
 import qualified Data.Text.Lazy
+import Clay.Size (Angle, Deg, LengthUnit, Size, deg, cm, mm, inches, px, pt, pc)
+import Clay.Transform
+import Clay (Number)
+import Data.Foldable (fold)
 
 newtype ArbitraryCss = ArbitraryCss Css
 
@@ -21,11 +25,22 @@ instance Show ArbitraryCss where
 instance Arbitrary ArbitraryCss where
   arbitrary = ArbitraryCss <$> genCss
 
-genAspectRatio :: Gen Css
-genAspectRatio = do
-  r <- uncurry (%) . bimap getPositive getPositive <$> (arbitrary :: Gen(Positive Integer, Positive Integer))
-  return $ aspectRatio (ratio r)
+-- | Generates a CSS document.
+genCss :: Gen Css
+genCss = do
+  p <- genProperties
+  s <- genSelector
+  combinator <- elements [root, (?), (<?)]
+  return (s `combinator` p)
 
+-- | Generate a CSS document containing zero or more properties.
+genProperties :: Gen Css
+genProperties = do
+  propGens <- sublistOf [genMargin, genAspectRatio, genTransforms]
+  props <- sequence propGens
+  return $ fold props
+
+-- | Generates a completely random selector.
 genSelector :: Gen Selector
 genSelector = sized genSelectorTree
  where
@@ -38,22 +53,52 @@ genSelector = sized genSelectorTree
     tree2 <- genSelectorTree (size `div` 2)
     return (tree1 `ctr` tree2)
 
-genCss :: Gen Css
-genCss = do
-  p <- genAspectRatio
-  s <- genSelector
-  combinator <- elements [root, (?), (<?)]
-  return (s `combinator` p)
+-- | Generates a CSS document containing a single aspect-ratio property.
+genAspectRatio :: Gen Css
+genAspectRatio = do
+  r <- uncurry (%) . bimap getPositive getPositive <$> (arbitrary :: Gen (Positive Integer, Positive Integer))
+  return $ aspectRatio (ratio r)
 
-test = mapM_ putCss =<< sample' genCss
+-- | Generates a transform property.
+genTransforms :: Gen Css
+genTransforms = do
+  scales <- mapM (<$> genNumber) [scaleX, scaleY, scaleZ]
+  rotates <- mapM (<$> genAngleDeg) [rotateX, rotateY, rotateZ]
+  translates <- mapM (<$> genSize) [translateX, translateY, translateZ]
+  transformations <- sublistOf (scales <> rotates <> translates)
+  return $ transforms transformations
 
+-- | Generates a margin in a random direction.
+genMargin :: Gen Css
+genMargin = do
+  marginFn <- elements [marginTop, marginLeft, marginRight, marginBottom]
+  marginFn <$> genSize
+
+-- | Generates a size with a random unit.
+genSize :: Gen (Size LengthUnit)
+genSize = do
+  sizeFn <- elements [cm, mm, inches, px, pt, pc]
+  sizeFn <$> genNumber
+
+-- | Generates a random fixed-precision (to 5 decimal places) numbe
+genNumber :: Gen Number
+genNumber = realToFrac <$> (arbitrary :: Gen Int)
+
+genAngleDeg :: Gen (Angle Deg)
+genAngleDeg = deg <$> genNumber
+
+-- | Generates selectors that terminate.
 genSelectorLeaf :: Gen Selector
 genSelectorLeaf = oneof [genElement, genStar]
 
+-- | Element names to generate.
+elementNames :: [String]
 elementNames = ["h1", "p", "div", "form", "br", "plupp", "onion", "span", "ng-for"]
 
+-- | Chooses between a set of element names to generate.
 genElement :: Gen Selector
 genElement = element . T.pack <$> elements elementNames
 
+-- | Always generates a star selector.
 genStar :: Gen Selector
 genStar = pure star
